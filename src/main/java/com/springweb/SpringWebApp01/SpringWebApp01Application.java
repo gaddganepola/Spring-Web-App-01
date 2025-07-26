c59cd22aafae6af6ff489b0092ce3c8f4a7fda28
gaddganepola <ganeypola@gmail.com>
2025-07-26 13:21:29 +0530

    project initialization

---
+++ src/main/java/com/springweb/SpringWebApp01/SpringWebApp01Application.java
@@ -0,0 +1,13 @@
+package com.springweb.SpringWebApp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebApp01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springweb/SpringWebApp01/SpringWebApp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springweb.SpringWebApp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
