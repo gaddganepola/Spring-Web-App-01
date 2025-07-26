e45b49a7c618016429fb7468cf8331182e1f9dd7
gaddganepola <ganeypola@gmail.com>
2025-07-26 15:56:08 +0530

    create the controller

---
+++ src/main/java/com/springweb/SpringWebApp01/HomeController.java
@@ -0,0 +1,11 @@
+package com.springweb.SpringWebApp01;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+    public String home() {
+        return "index.jsp";
+    }
+}
