f8e9bfedc1eef3d2fd75c8c974d150ec0fc6c653
gaddganepola <ganeypola@gmail.com>
2025-07-26 16:42:27 +0530

    access the values coming from the request and create new jsp file to show the results

--- src/main/java/com/springweb/SpringWebApp01/HomeController.java
+++ src/main/java/com/springweb/SpringWebApp01/HomeController.java
@@ -1,5 +1,6 @@
 package com.springweb.SpringWebApp01;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,5 +10,14 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req) {
+        int num1= Integer.parseInt(req.getParameter("num1"));
+        int num2= Integer.parseInt(req.getParameter("num2"));
+        int result= num1 + num2;
+        System.out.println(result);
+        return "add.jsp";
     }
 }
---
+++ src/main/webapp/add.jsp
@@ -0,0 +1,11 @@
+<%@page language="java" %>
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Result is: </h1>
+  </div>
+</body>
+</html>
